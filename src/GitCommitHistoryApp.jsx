876150670450a803ee10ddf4d7c55c05e5f48fd7
Jean Centeno <jeancenteno54@gmail.com>
2023-11-06 19:09:33 -0300

    tailwind was implemented

--- src/GitCommitHistoryApp.jsx
+++ src/GitCommitHistoryApp.jsx
@@ -1,32 +1,65 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 export const GitCommitHistoryApp = () => {
+  const [username, setUsername] = useState("");
+  const [repository, setRepository] = useState("");
   const [commits, setCommits] = useState([]);
 
-  useEffect(() => {
-    const fetchCommits = async () => {
-      try {
-        const response = await fetch("http://localhost:4000/github/commits"); // Debes ajustar la URL según tu configuración de rutas en NestJS
-        if (response.ok) {
-          const data = await response.json();
-          setCommits(data);
-        } else {
-          throw new Error("No se pudo obtener el historial de commits.");
-        }
-      } catch (error) {
-        console.error(
-          "Error al obtener el historial de commits activo:",
-          error
-        );
-      }
-    };
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
 
-    fetchCommits();
-  }, []);
+  const handleRepositoryChange = (e) => {
+    setRepository(e.target.value);
+  };
+
+  console.log(commits);
+
+  const fetchCommits = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:4000/github/commits/${username}/${repository}`
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setCommits(data);
+      } else {
+        throw new Error("No se pudo obtener el historial de commits.");
+      }
+    } catch (error) {
+      console.error("Error al obtener el historial de commits:", error);
+    }
+  };
 
   return (
-    <div className="App">
-      <h1>Historial de Commits de GitHub</h1>
+    <div className=" flex flex-col items-center">
+      <h1 className="text-center font-bold text-lg">Git Commits History</h1>
+      <div className="flex flex-col items-center mt-4">
+        <div className="flex gap-4">
+          <input
+            type="text"
+            placeholder="GitHub Username"
+            value={username}
+            onChange={handleUsernameChange}
+            className="w-[200px] px-1"
+          />
+          <input
+            type="text"
+            placeholder="Repository Name"
+            value={repository}
+            onChange={handleRepositoryChange}
+            className="w-[200px] px-1"
+          />
+        </div>
+
+        <button
+          onClick={fetchCommits}
+          className="bg-blue-300 hover:bg-blue-400 flex w-fit px-2 py-1 mt-2"
+        >
+          <span className="font-semibold">Obtener Commits</span>
+        </button>
+      </div>
       {commits?.map((commit) => (
         <div key={commit.sha}>
           <p>SHA: {commit.sha}</p>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./input.css";
 import { GitCommitHistoryApp } from "./GitCommitHistoryApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
